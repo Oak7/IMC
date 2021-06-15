735b75f2b2a503381c89287926ea9d2042cdc59f
Gabriela <aragao.gabi@gmail.com>
2021-06-14 22:39:18 -0300

    Definição de peso e altura

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace IMC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe o peso em Kg: ");
+            double peso = double.Parse(Console.ReadLine());
+
+            Console.Write("Informe sua altura em metros");
+            double altura = double.Parse(Console.ReadLine());
+
+            double IMC = peso /(altura* altura);
+        }
+    }
+}
