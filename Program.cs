633daa76d1a453941bf53edae3744a336d7c6eb2
Gabriela <aragao.gabi@gmail.com>
2021-06-16 13:19:31 -0300

    Versão Final

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 
+
 namespace IMC
 {
     class Program
@@ -9,10 +10,32 @@ namespace IMC
             Console.Write("Informe o peso em Kg: ");
             double peso = double.Parse(Console.ReadLine());
 
-            Console.Write("Informe sua altura em metros");
+            Console.Write("Informe sua altura em metros: ");
             double altura = double.Parse(Console.ReadLine());
 
-            double IMC = peso /(altura* altura);
+            double IMC = peso /(altura*altura);
+
+            if(IMC < 20)
+            {
+                Console.WriteLine("\nIMC = {0:00} ---> abaixo do peso" , IMC);
+            }
+            else if((IMC >= 20) && (IMC <=24))
+            {
+                Console.WriteLine("IMC = {0:00} ---> normal" , IMC);
+            }
+            else if((IMC >= 25) && (IMC <=29))
+            {
+                Console.WriteLine("IMC = {0:00} ---> acima do peso" , IMC);
+            }
+            else if((IMC >= 30) && (IMC <=34))
+            {
+                Console.WriteLine("IMC = {0:00} ---> obeso",IMC);
+            }
+            else
+            {
+                Console.WriteLine("IMC = {0:00} ---> muito obeso",IMC);
+
+            }
         }
     }
 }
